b983744f8bad78e97496ccfdd65fb02d3a69a6f5
Brian Patino <patinobrian@gmail.com>
2016-11-01 23:09:06 -0400

    add commented protractor config

--- e2e-tests/protractor.conf.js
+++ e2e-tests/protractor.conf.js
@@ -3,21 +3,21 @@ exports.config = {
 
   allScriptsTimeout: 11000,
 
-  seleniumAddress: 'http://localhost:4444/wd/hub',
+  // seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: [
-    'scenarios.js'
+    '*.js'
   ],
 
-  /*capabilities: {
+  capabilities: {
     'browserName': 'chrome'
   },
 
-  // baseUrl: 'http://localhost:8000/',
+  baseUrl: 'http://localhost:8000/',
 
   framework: 'jasmine',
 
   jasmineNodeOpts: {
     defaultTimeoutInterval: 30000
-  }*/
+  }
 
 };
